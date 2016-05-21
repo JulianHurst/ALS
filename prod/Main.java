73c1c5a5cf206924b3863719fa9a3128af5976f5
JulianHurst <julian.hurst@laposte.net>
2016-05-21 03:38:46 +0200

    Print inutile

--- prod/Main.java
+++ prod/Main.java
@@ -2,9 +2,7 @@ import affichage.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test"+(char)9+"test");
+	public static void main(String[] args) {			
 		Als a1 = new Als();
 	}
 }
